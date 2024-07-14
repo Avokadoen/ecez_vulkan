48f078faacd8ebedebfc5f4b2dae167ce6c3cfa2
avokadoen <akselhallajs@gmail.com>
2024-07-14 14:29:35 +0200

    move some zgui code into core to share across game and editor

--- src/core.zig
+++ src/core.zig
@@ -1,3 +1,5 @@
 pub const AssetHandler = @import("core/AssetHandler.zig");
-pub const glfw_integration = @import("core/glfw_integration.zig");
 pub const component_reflect = @import("core/component_reflect.zig");
+
+pub const glfw_integration = @import("core/glfw_integration.zig");
+pub const zgui_integration = @import("core/zgui_intergration.zig");
--- src/editor/Editor.zig
+++ src/editor/Editor.zig
@@ -10,6 +10,7 @@ const tracy = @import("ztracy");
 const zm = @import("zmath");
 
 const core = @import("../core.zig");
+const marker = core.zgui_integration.marker;
 
 const EditorIcons = @import("EditorIcons.zig");
 const Icons = EditorIcons.Icon;
@@ -1098,7 +1099,7 @@ pub fn setEditorInput(window: glfw.Window) void {
             // zgui.addKeyEvent(zgui.Key.mod_caps_lock, mod.caps_lock);
             // zgui.addKeyEvent(zgui.Key.mod_num_lock, mod.num_lock);
 
-            zgui.io.addKeyEvent(mapGlfwKeyToImgui(input_key), action == .press);
+            zgui.io.addKeyEvent(core.zgui_integration.mapGlfwKeyToImgui(input_key), action == .press);
 
             const user_pointer = core.glfw_integration.findUserPointer(
                 UserPointer,
@@ -1385,158 +1386,4 @@ inline fn createNewEntityMenu(self: *Editor) !void {
             }
         }
     }
-}
-
-const MarkerType = enum {
-    warning,
-    hint,
-};
-inline fn marker(message: []const u8, marker_type: MarkerType) void {
-    const zone = tracy.ZoneN(@src(), @src().fn_name);
-    defer zone.End();
-
-    const marker_txt = switch (marker_type) {
-        .warning => "(!)",
-        .hint => "(?)",
-    };
-    zgui.textDisabled(marker_txt, .{});
-    if (zgui.isItemHovered(.{})) {
-        if (zgui.beginTooltip()) {
-            defer zgui.endTooltip();
-
-            zgui.pushTextWrapPos(zgui.getFontSize() * 35);
-            zgui.textUnformatted(message);
-            zgui.popTextWrapPos();
-        }
-    }
-}
-
-inline fn mapGlfwKeyToImgui(key: glfw.Key) zgui.Key {
-    const zone = tracy.ZoneN(@src(), @src().fn_name);
-    defer zone.End();
-
-    return switch (key) {
-        .unknown => zgui.Key.none,
-        .space => zgui.Key.space,
-        .apostrophe => zgui.Key.apostrophe,
-        .comma => zgui.Key.comma,
-        .minus => zgui.Key.minus,
-        .period => zgui.Key.period,
-        .slash => zgui.Key.slash,
-        .zero => zgui.Key.zero,
-        .one => zgui.Key.one,
-        .two => zgui.Key.two,
-        .three => zgui.Key.three,
-        .four => zgui.Key.four,
-        .five => zgui.Key.five,
-        .six => zgui.Key.six,
-        .seven => zgui.Key.seven,
-        .eight => zgui.Key.eight,
-        .nine => zgui.Key.nine,
-        .semicolon => zgui.Key.semicolon,
-        .equal => zgui.Key.equal,
-        .a => zgui.Key.a,
-        .b => zgui.Key.b,
-        .c => zgui.Key.c,
-        .d => zgui.Key.d,
-        .e => zgui.Key.e,
-        .f => zgui.Key.f,
-        .g => zgui.Key.g,
-        .h => zgui.Key.h,
-        .i => zgui.Key.i,
-        .j => zgui.Key.j,
-        .k => zgui.Key.k,
-        .l => zgui.Key.l,
-        .m => zgui.Key.m,
-        .n => zgui.Key.n,
-        .o => zgui.Key.o,
-        .p => zgui.Key.p,
-        .q => zgui.Key.q,
-        .r => zgui.Key.r,
-        .s => zgui.Key.s,
-        .t => zgui.Key.t,
-        .u => zgui.Key.u,
-        .v => zgui.Key.v,
-        .w => zgui.Key.w,
-        .x => zgui.Key.x,
-        .y => zgui.Key.y,
-        .z => zgui.Key.z,
-        .left_bracket => zgui.Key.left_bracket,
-        .backslash => zgui.Key.back_slash,
-        .right_bracket => zgui.Key.right_bracket,
-        .grave_accent => zgui.Key.grave_accent,
-        .world_1 => zgui.Key.none, // ????
-        .world_2 => zgui.Key.none, // ????
-        .escape => zgui.Key.escape,
-        .enter => zgui.Key.enter,
-        .tab => zgui.Key.tab,
-        .backspace => zgui.Key.back_space,
-        .insert => zgui.Key.insert,
-        .delete => zgui.Key.delete,
-        .right => zgui.Key.right_arrow,
-        .left => zgui.Key.left_arrow,
-        .down => zgui.Key.down_arrow,
-        .up => zgui.Key.up_arrow,
-        .page_up => zgui.Key.page_up,
-        .page_down => zgui.Key.page_down,
-        .home => zgui.Key.home,
-        .end => zgui.Key.end,
-        .caps_lock => zgui.Key.caps_lock,
-        .scroll_lock => zgui.Key.scroll_lock,
-        .num_lock => zgui.Key.num_lock,
-        .print_screen => zgui.Key.print_screen,
-        .pause => zgui.Key.pause,
-        .F1 => zgui.Key.f1,
-        .F2 => zgui.Key.f2,
-        .F3 => zgui.Key.f3,
-        .F4 => zgui.Key.f4,
-        .F5 => zgui.Key.f5,
-        .F6 => zgui.Key.f6,
-        .F7 => zgui.Key.f7,
-        .F8 => zgui.Key.f8,
-        .F9 => zgui.Key.f9,
-        .F10 => zgui.Key.f10,
-        .F11 => zgui.Key.f11,
-        .F12 => zgui.Key.f12,
-        .F13,
-        .F14,
-        .F15,
-        .F16,
-        .F17,
-        .F18,
-        .F19,
-        .F20,
-        .F21,
-        .F22,
-        .F23,
-        .F24,
-        .F25,
-        => zgui.Key.none,
-        .kp_0 => zgui.Key.keypad_0,
-        .kp_1 => zgui.Key.keypad_1,
-        .kp_2 => zgui.Key.keypad_2,
-        .kp_3 => zgui.Key.keypad_3,
-        .kp_4 => zgui.Key.keypad_4,
-        .kp_5 => zgui.Key.keypad_5,
-        .kp_6 => zgui.Key.keypad_6,
-        .kp_7 => zgui.Key.keypad_7,
-        .kp_8 => zgui.Key.keypad_8,
-        .kp_9 => zgui.Key.keypad_9,
-        .kp_decimal => zgui.Key.keypad_decimal,
-        .kp_divide => zgui.Key.keypad_divide,
-        .kp_multiply => zgui.Key.keypad_multiply,
-        .kp_subtract => zgui.Key.keypad_subtract,
-        .kp_add => zgui.Key.keypad_add,
-        .kp_enter => zgui.Key.keypad_enter,
-        .kp_equal => zgui.Key.keypad_equal,
-        .left_shift => zgui.Key.left_shift,
-        .left_control => zgui.Key.left_ctrl,
-        .left_alt => zgui.Key.left_alt,
-        .left_super => zgui.Key.left_super,
-        .right_shift => zgui.Key.right_shift,
-        .right_control => zgui.Key.right_ctrl,
-        .right_alt => zgui.Key.right_alt,
-        .right_super => zgui.Key.right_super,
-        .menu => zgui.Key.menu,
-    };
 }
